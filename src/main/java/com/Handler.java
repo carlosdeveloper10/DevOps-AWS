b8357f21940dbae4a20012bda922820c86ee5204
carlosdeveloper10 <>
2018-09-02 20:00:35 -0500

    Se recibe informacion para ser almacenada

---
+++ src/main/java/com/Handler.java
@@ -0,0 +1,25 @@
+package com;
+
+import com.domain.Client;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Client, String> {
+
+    @Override
+    public String handleRequest(Client client, Context context) {
+    	SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(client);
+            session.getTransaction().commit();
+        }
+
+        return String.format("Added %s %s.", client.getId(), client.getName());
+    }
+}
+
---
+++ src/main/java/com/domain/Client.java
@@ -0,0 +1,26 @@
+package com.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+public class Client {
+    private int id;
+    private String name;
+
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
